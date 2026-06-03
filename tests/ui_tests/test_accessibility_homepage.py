7917b4203d254e9af5197611c491d38a3f60e3a1
wojtalo94 <wojtek94@interia.eu>
2026-06-03 12:38:58 +0200

    First changes for accessibility test

---
+++ tests/ui_tests/test_accessibility_homepage.py
@@ -0,0 +1,11 @@
+import pytest
+
+
+@pytest.mark.accessibility
+def test_login_accessibility(logged_in_page, check_accessibility):
+    page = logged_in_page
+
+    confirm_button = page.get_by_test_id("create-board-submit-button")
+    confirm_button.wait_for(state="visible", timeout=15000)
+    
+    check_accessibility()
