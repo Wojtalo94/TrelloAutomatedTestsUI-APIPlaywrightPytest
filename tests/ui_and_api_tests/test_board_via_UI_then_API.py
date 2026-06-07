d35a0a5f04bb0ac4b42f2329d02484ee54ba224c
wojtalo94 <wojtek94@interia.eu>
2026-06-07 11:22:38 +0200

    Small test name change

--- tests/ui_and_api_tests/test_board_via_UI_then_API.py
+++ tests/ui_and_api_tests/test_board_via_UI_then_API.py
@@ -3,7 +3,7 @@ import time
 from pages.board_page import BoardPage
 
 @pytest.mark.ui_and_api
-def test_create_board_via_UI_and_verify_in_api(rest_controller, logged_in_page):
+def test_create_board_in_UI_and_verify_in_api(rest_controller, logged_in_page):
     board_page = BoardPage(logged_in_page)
     board_name = f"hybrid_UI_API_test_board_{int(time.time())}"
 
